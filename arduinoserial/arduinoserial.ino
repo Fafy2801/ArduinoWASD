dda6fdd936705d8ef2a1d20d80f12179a113f38b
Fafy <fabianrojas2801@gmail.com>
2020-11-30 20:48:11 -0300

    Arduino writes to serial depending on pins

--- arduinoserial/arduinoserial.ino
+++ arduinoserial/arduinoserial.ino
@@ -1,10 +1,27 @@
 int pins[] = { 3, 4, 5, 6 }; // WASD
-bool reports[] = { false, false, false, false }; // true if pin was HIGH
 
 void setup() {
-  
+  Serial.begin(9600);
+  // Initialize pins
+  for (byte i = 0; i < sizeof(pins) / sizeof(pins[0]); i++) {
+    pinMode(pins[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  
+  // Check for pin reading and write to serial
+  for (byte i = 0; i < sizeof(pins) / sizeof(pins[0]); i++) {
+    long reading = digitalRead(pins[i]);
+
+    if (reading == HIGH) {
+      Serial.write("0");
+    }
+    else {
+      Serial.write("1");
+    }
+  }
+
+  // Finish sending serial for this loop
+  Serial.println();
+  delay(100);
 }
