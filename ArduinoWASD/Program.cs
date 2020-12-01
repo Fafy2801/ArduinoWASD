f6df8add21414425070794e822706dfd22a66cbe
Fafy <fabianrojas2801@gmail.com>
2020-11-30 21:06:33 -0300

    Connect to arduino

--- ArduinoWASD/Program.cs
+++ ArduinoWASD/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace ArduinoWASD
 {
     class Program
     {
+        static SerialPort arduino;
+
         static void Main(string[] args)
         {
+        // Get COM
+        PortInit:
+            Console.WriteLine("Input COM port:");
+
+            string port = Console.ReadLine();
+            // Open port
+            try
+            {
+                if (port.Length == 1)
+                    arduino = new SerialPort("COM" + port);
+                else
+                    arduino = new SerialPort(port);
+                arduino.Open();
+
+                Console.WriteLine("Using port: " + arduino.PortName);
+            }
+            // Retry
+            catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+                Console.WriteLine();
+
+                goto PortInit;
+            }
         }
     }
 }
