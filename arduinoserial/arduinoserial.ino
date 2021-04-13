484edabf6b5ba3238b8fc304e6778764d37d7bdf
Fafy <fabianrojas2801@gmail.com>
2021-04-12 23:41:22 -0300

    Updated correct arduino file

--- arduinoserial/arduinoserial.ino
+++ arduinoserial/arduinoserial.ino
@@ -1,27 +1,41 @@
-int pins[] = { 3, 4, 5, 6 }; // WASD
+#include<CapacitiveSensor.h>
+
+int PIN_OUT = 3;
+
+int pins[4] = {5, 6, 10, 11};
+int leds[4] = {8, 9, 12, 4};
+
+int iterations = sizeof(pins) / sizeof(int);
+
+CapacitiveSensor sensors[] = {};
 
 void setup() {
   Serial.begin(9600);
-  // Initialize pins
-  for (byte i = 0; i < sizeof(pins) / sizeof(pins[0]); i++) {
-    pinMode(pins[i], INPUT_PULLUP);
+
+  for(int i = 0; i < iterations; i++)
+    pinMode(leds[i], OUTPUT);
+
+  for(int i = 0; i < iterations; i++){
+      sensors[i] = CapacitiveSensor(PIN_OUT, pins[i]);
+      sensors[i].set_CS_AutocaL_Millis(0xFFFFFFFF);
   }
 }
 
-void loop() {
-  // Check for pin reading and write to serial
-  for (byte i = 0; i < sizeof(pins) / sizeof(pins[0]); i++) {
-    long reading = digitalRead(pins[i]);
+long reading = 0;
+byte out = 0;
 
-    if (reading == HIGH) {
-      Serial.write("0");
-    }
-    else {
-      Serial.write("1");
-    }
+void loop() {
+  out = 0;
+  for(int i = 0; i < iterations; i++){
+    reading = sensors[i].capacitiveSensor(160);
+
+    if(reading > 25){
+      bitSet(out, i);
+      digitalWrite(leds[i], HIGH);
+    } else
+      digitalWrite(leds[i], LOW);
   }
 
-  // Finish sending serial for this loop
-  Serial.println();
-  delay(25);
+  Serial.write(out);
+  //delay(10);
 }
